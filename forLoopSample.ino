a63e69fe6eebc88c54debae5fa8a89fe437050d5
Nadica Miljković <41929315+NadicaSm@users.noreply.github.com>
2018-12-09 20:34:43 +0100

    Add files via upload

---
+++ forLoopSample.ino
@@ -0,0 +1,18 @@
+const int kPinLed = 13;
+
+void setup() {
+  pinMode(kPinLed, OUTPUT); 
+}
+
+void loop() {
+  for (int ind = 0; ind < 5; ind++){
+    digitalWrite(kPinLed, HIGH);
+    delay(150);
+    digitalWrite(kPinLed, LOW);
+    delay(150);
+  }
+  delay(1500);
+}
+
+
+
---
+++ userInteractivity.ino
@@ -0,0 +1,25 @@
+/*
+  Sample user interface.
+  User enters number on serial port.
+*/
+int Num; 
+
+void setup() {
+  // initialize serial communication and set data rate for serial
+  // data communication
+  Serial.begin(9600);
+}
+
+void loop() {
+  // print inquiry at the serial port (see Serial monitor)
+  Serial.println("Enter an integer: ");
+  while(Serial.available() == 0) { // wait for user to enter number
+  }
+  Num = Serial.parseInt();  // read number from serial port
+  // Print entered value
+  Serial.print("You entered: ");
+  Serial.println(Num);
+}
+
+
+
